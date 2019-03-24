c1ece89b689c7c7885f9696a18ebffc047e0c341
Yoshanth Raaj <48622984+mryoshanth@users.noreply.github.com>
2019-03-24 16:59:43 +0800

    Add files via upload

--- Guess the number.py
+++ Guess the number.py
@@ -4,8 +4,8 @@ again = True
 while again:
     guess_taken = 0
     number = random.randint(1, 5)
-    print("Guess the number between 1 to 5")
-    user_input = input()
+    print("\nGuess the number between 1 to 5")
+    user_input = input("Number (1-5):")
     guess_taken += 1
     try:
         user_input = int(user_input)
@@ -14,14 +14,12 @@ while again:
         continue
     while guess_taken < 9:
         if user_input > number:
-           print("   ")
-           print("That's too high, try again.")
-           user_input = int(input())
+           print("\nThat's too high, try again.")
+           user_input = int(input("Number (1-5):"))
            guess_taken += 1
         elif user_input < number:
-           print("   ")
-           print("That's too low, try again.")
-           user_input = int(input())
+           print("\nThat's too low, try again.")
+           user_input = int(input("Number (1-5):"))
            guess_taken += 1
         else:
             print("   ")
@@ -34,7 +32,7 @@ while again:
     else:
         print("You have reached the maximum number of tries.")
 
-    print("Do you wanna play again?")
+    print("\nDo you wanna play again?")
     again = ("y" or "yes") in input().lower()
 
 print("Good game, see ya!")
